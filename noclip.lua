57971084ae378a5aa1b6c432539466b949d645b3
lbw1379 <119725607+lbw1379@users.noreply.github.com>
2023-02-13 22:13:00 +0800

    Update noclip.lua

--- noclip.lua
+++ noclip.lua
@@ -31,7 +31,7 @@ Title.BorderColor3 = Color3.new(0.180392, 0, 0.431373)
 Title.BorderSizePixel = 2
 Title.Size = UDim2.new(0, 210, 0, 33)
 Title.Font = Enum.Font.Highway
-Title.Text = "穿墙"
+Title.Text = "Title"
 Title.TextColor3 = Color3.new(1, 1, 1)
 Title.FontSize = Enum.FontSize.Size32
 Title.TextSize = 30
@@ -67,7 +67,7 @@ StatusPF.TextStrokeColor3 = Color3.new(0.333333, 0.333333, 0.333333)
 StatusPF.TextStrokeTransparency = 0
 StatusPF.TextWrapped = true
 
-Status.Name = "启动"
+Status.Name = "on"
 Status.Parent = BG
 Status.BackgroundColor3 = Color3.new(1, 1, 1)
 Status.BackgroundTransparency = 1
@@ -99,9 +99,9 @@ Credit.TextStrokeTransparency = 0
 Credit.TextWrapped = true
 
 Toggle.MouseButton1Click:connect(function()
-	if Status.Text == "关" then
+	if Status.Text == "off" then
 		Clipon = true
-		Status.Text = "开"
+		Status.Text = "on"
 		Status.TextColor3 = Color3.new(0,185,0)
 		Stepped = game:GetService("RunService").Stepped:Connect(function()
 			if not Clipon == false then
@@ -115,9 +115,9 @@ Toggle.MouseButton1Click:connect(function()
 				Stepped:Disconnect()
 			end
 		end)
-	elseif Status.Text == "开" then
+	elseif Status.Text == "on" then
 		Clipon = false
-		Status.Text = "关"
+		Status.Text = "off"
 		Status.TextColor3 = Color3.new(170,0,0)
 	end
 end)
